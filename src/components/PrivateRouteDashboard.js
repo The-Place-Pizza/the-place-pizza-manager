a1fc2512b0d9b47bc1b9975ac8ca6e86a589a922
Will G <wgrout87@gmail.com>
2022-11-20 16:37:39 -0700

    refactored code

--- src/components/PrivateRouteDashboard.js
+++ src/components/PrivateRouteDashboard.js
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import Dashboard from "./Dashboard";
 
-export default function PrivateRoute({ props: Component }) {
+export default function PrivateRoute() {
     const { currentUser } = useAuth();
 
     return currentUser ? <Dashboard /> : <Navigate to='/login' />
--- src/components/PrivateRouteUpdateProfile.js
+++ src/components/PrivateRouteUpdateProfile.js
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import UpdateProfile from "./UpdateProfile";
 
-export default function PrivateRoute({ props: Component }) {
+export default function PrivateRoute() {
     const { currentUser } = useAuth();
 
     return currentUser ? <UpdateProfile /> : <Navigate to='/login' />
