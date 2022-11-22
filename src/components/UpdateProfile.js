b0761ab8476f772e8bcdab3493ec58777bdd5ded
Will G <wgrout87@gmail.com>
2022-11-21 23:13:01 -0700

    added update profile functionality

--- src/components/UpdateProfile.js
+++ src/components/UpdateProfile.js
@@ -7,28 +7,36 @@ export default function UpdateProfile() {
     const emailRef = useRef();
     const passwordRef = useRef();
     const passwordConfirmRef = useRef();
-    const { currentUser } = useAuth();
+    const { currentUser, updateEmail, updatePassword } = useAuth();
     const [error, setError] = useState('');
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
-    async function handleSubmit(e) {
-        // e.preventDefault();
+    function handleSubmit(e) {
+        e.preventDefault();
 
-        // if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-        //     return setError('Passwords do not match');
-        // }
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+            return setError('Passwords do not match');
+        }
 
-        // try {
-        //     setError('');
-        //     setLoading(true);
-        //     await signup(emailRef.current.value, passwordRef.current.value);
-        //     navigate('/dashboard');
-        // } catch {
-        //     setError('Failed to create an account');
-        // };
+        const promises = [];
+        setLoading(true);
+        setError('');
 
-        // setLoading(false);
+        if (emailRef.current.value !== currentUser.email) {
+            promises.push(updateEmail(emailRef.current.value));
+        }
+        if (passwordRef.current.value) {
+            promises.push(updatePassword(passwordRef.current.value));
+        }
+
+        Promise.all(promises).then(() => {
+            navigate('/')
+        }).catch(() => {
+            setError('Failed to update account');
+        }).finally(() => {
+            setLoading(false);
+        })
     }
 
     return (
